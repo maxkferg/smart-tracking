b07bee36cab0a704d12f04ec055dc68e4e53dee5
Max Ferguson <maxkferg@gmail.com>
2017-11-11 20:34:01 -0800

    Added debugging tools

---
+++ test/config.py
@@ -0,0 +1,8 @@
+
+# Ip webcam details
+CAMERA_IP = 1
+
+# Redis details
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
---
+++ test/position.py
@@ -0,0 +1,22 @@
+"""
+Monitor the redis database and print the position when it changes
+
+Usage: python3 track.py
+"""
+
+import redis
+import time
+import json
+from .config import *
+
+db = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+
+position = None
+
+while True:
+	new_position = db.get('position')
+	if new_position and new_position!=position:
+		print(json.loads(new_position))
+	# Prepare for next loop
+	position = new_position
+	time.sleep(0.01)
